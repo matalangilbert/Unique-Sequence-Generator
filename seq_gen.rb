939e87571352c68d67e6a7b9093b072ccf50107a
Mat Gilbert <freefallertam@gmail.com>
2012-04-01 17:32:43 +0100

    Frequecy analysis working with hashes

--- seq_gen.rb
+++ seq_gen.rb
@@ -31,35 +31,17 @@ sequences.delete_if do |seq|
   (seq.first == seq.second) || (seq.first == seq.third) || (seq.first == seq.fourth) || (seq.second == seq.third) || (seq.second == seq.fourth) || (seq.third == seq.fourth)
 end
 
-first_frequencies = Array.new
-
-for i in 0..9 do
-  first_frequencies << 0
-end
+frequencies = Hash.new(0)
  
+def get_frequencies(frequencies,position,number)
+  frequencies[[position,number]] += 1
+end
+
 sequences.each do |sequence|
-  case sequence.first
-  when 0
-    first_frequencies[0] += 1
-  when 1
-    first_frequencies[1] += 1
-  when 2
-    first_frequencies[2] += 1
-  when 3
-    first_frequencies[3] += 1
-  when 4
-    first_frequencies[4] += 1
-  when 5
-    first_frequencies[5] += 1
-  when 6
-    first_frequencies[6] += 1
-  when 7
-    first_frequencies[7] += 1
-  when 8
-    first_frequencies[8] += 1
-  when 9
-    first_frequencies[9] += 1
-  end
+  get_frequencies(frequencies,1,sequence.first)
+  get_frequencies(frequencies,2,sequence.second)
+  get_frequencies(frequencies,3,sequence.third)  
+  get_frequencies(frequencies,4,sequence.fourth)
 end
 
 sequences.each do |sequence|
@@ -68,11 +50,7 @@ end
 
 puts "Number of sequences: #{sequences.length}"
 
-i = 0
-first_frequencies.each do |frequency|
-  puts "First frequency of #{i}: #{frequency}"
-  i += 1
-end
+puts "Frequencies: #{frequencies.inspect}"
 
 
 
