2af815fa680f146b71c5b3594b73c6fe5c65540a
Mat Gilbert <freefallertam@gmail.com>
2012-04-02 13:33:16 +0100

    Working for 20 samples

--- seq_gen.rb
+++ seq_gen.rb
@@ -2,37 +2,73 @@
 
 sequences = Array.new
 
-def new_sample(samples)
+def new_sample(first_samples, second_samples, third_samples, fourth_samples)
 
-  one = samples.sample
- 
+  one = first_samples.sample
+  
   begin
-    two = samples.sample
+    two = second_samples.sample
   end while (two == one)
 
   begin
-    three = samples.sample
+    three = third_samples.sample
   end while ((three == two) || (three == one))
 
   begin
-    four = samples.sample
+    four = fourth_samples.sample
   end while ((four == three) || (four == two) || (four == one))
   
+  for i in 0..first_samples.length
+    if (first_samples[i] == one)
+      first_samples.delete_at(i)
+      break
+    end
+  end
+  
+  for i in 0..second_samples.length
+    if (second_samples[i] == two) 
+      second_samples.delete_at(i)
+      break
+    end
+  end
+  
+  for i in 0..third_samples.length
+    if (third_samples[i] == three)
+      third_samples.delete_at(i)
+      break
+    end
+  end
+  
+  for i in 0..fourth_samples.length
+    if (fourth_samples[i] == four)
+      fourth_samples.delete_at(i)
+      break
+    end
+  end
+
   sample = "#{one}#{two}#{three}#{four}"
 end
 
+first_position_samples = (0..9).to_a + (0..9).to_a
+second_position_samples = (0..9).to_a + (0..9).to_a
+third_position_samples =  (0..9).to_a + (0..9).to_a
+fourth_position_samples =  (0..9).to_a + (0..9).to_a
 
-samples = (0..9).to_a
-while(sequences.length < 100)
-  sample = new_sample(samples)
+while(sequences.length <18)
+  sample = new_sample(first_position_samples, second_position_samples, third_position_samples, fourth_position_samples)
   unless (sequences.include?(sample))
     sequences << sample
+    puts "Sample: #{sample}. Total sequences: #{sequences.length}"
   end
 end
 
-unless (sequences.uniq!)
+unless sequences.uniq!
   puts "All unique sequences!"
 end
 
+sequences.each do |seq|
+  array = seq.chars.to_a
+  puts array.inspect
+end
 puts "Sequences: #{sequences.inspect}"
 puts "Number of Sequences: #{sequences.length}"
