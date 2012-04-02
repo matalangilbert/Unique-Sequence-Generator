6c2fd14c5e43a662a0ef67ac1133f8a2b3a57979
Mat Gilbert <freefallertam@gmail.com>
2012-04-02 19:46:18 +0100

    Updated comments

--- seq_gen.rb
+++ seq_gen.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 =begin
-Author:   Mathew Gilbert freefallertam _AT_ gmail _DOT_ com
-Filename: seq_gen.rb
-Date:     2-April-2012
-Summary:  Calculates unique 4 digit long number sequences where
-each position (1 through 4) contains digits (0 through 9)
-the same number of times. Outputs a .csv file for each sequence.
-Calculates sequences from 10 digits long to 1000 digits long (starts
-running more slowly after 100 digits long, currently tested to
-correctly output 300 digit long sequence).
+  * Name: seq_gen.rb
+  * Author:   Mathew Gilbert freefallertam _AT_ gmail _DOT_ com
+  * Summary:  Calculates unique 4 digit long number sequences where
+    each position (1 through 4) contains digits (0 through 9)
+    the same number of times. Outputs a .csv file for each sequence.
+    Calculates sequences from 10 digits long to 1000 digits long (starts
+    running more slowly after 100 digits long, currently tested to
+    correctly output 300 digit long sequence).
+  * Date:     2-April-2012
 =end
 
 =begin
-Input:    sequences => an array of sequences 4 digits long
-Returns:  two-dimensional array [position][digit] = frequency of digit occurrence
+  * Input:    sequences => an array of sequences 4 digits long
+  * Returns:  two-dimensional array [position][digit] = frequency of digit occurrence
 =end
 def calcuate_frequencies(sequences)
 freq_array = Array.new(4) {|i|Array.new(10){|i| 0}}
@@ -26,25 +26,25 @@ freq_array
 end
 
 =begin
-Returns:  array of all permutations of numbers 0 to 9, 4 digits long
+  * Returns:  array of all permutations of numbers 0 to 9, 4 digits long
 =end
 def generate_sequence()
 samples = (0..9).to_a.permutation(4).to_a
 end
 
 =begin
-Input:    frequencies => two-dimensional array ([position][digit] = frequency of digit occurrence)
-digit => the digit to be queried (1 through 4)
-position => the position of the digit to be queried
-Returns:  the frequency of occurrence of the digit at position
+  * Input:  frequencies => two-dimensional array ([position][digit] = frequency of digit occurrence)
+            digit => the digit to be queried (1 through 4)
+            position => the position of the digit to be queried
+  * Returns:  the frequency of occurrence of the digit at position
 =end
 def get_frequency(frequencies,digit,position)
 frequencies[position][digit]
 end
 
 =begin
-Input:    str => string to be evaluated
-Returns:  boolean => true if string evaluates to positive integer, otherwise false.
+  * Input:    str => string to be evaluated
+  * Returns:  boolean => true if string evaluates to positive integer, otherwise false.
 =end
 def is_int?(str)
 !!(str =~ /^[+]?[0-9]+$/) #regexp for positive integer, returning boolean
