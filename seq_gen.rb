025aaf00bc50300d328798cd9240d0cf8bd002a8
Mat Gilbert <freefallertam@gmail.com>
2012-04-02 01:56:35 +0100

    Working for initial sequence

--- seq_gen.rb
+++ seq_gen.rb
@@ -31,24 +31,19 @@ sequences.delete_if do |seq|
   (seq.first == seq.second) || (seq.first == seq.third) || (seq.first == seq.fourth) || (seq.second == seq.third) || (seq.second == seq.fourth) || (seq.third == seq.fourth)
 end
 
-frequencies = Hash.new(0)
-
-sequences.each do |sequence|
-  frequencies[[1,sequence.first]] += 1
-  frequencies[[2,sequence.second]] += 1
-  frequencies[[3,sequence.third]] += 1
-  frequencies[[4,sequence.fourth]] += 1
-  puts "#{sequence}"
-end
-
 final_sequences = Array.new
+final_frequencies = Hash.new(0)
 
 LIMIT=7
-def add_sequence(frequencies, sequence)  
+def add_sequence(final_sequences,frequencies, sequence)  
   if ((frequencies[[1,sequence.first]]+1)<LIMIT)
     if (frequencies[[2,sequence.second]]<LIMIT)
       if (frequencies[[3,sequence.third]]<LIMIT)
         if (frequencies[[4,sequence.fourth]]<LIMIT)
+          frequencies[[1,sequence.first]] += 1
+          frequencies[[2,sequence.second]] += 1
+          frequencies[[3,sequence.third]] += 1
+          frequencies[[4,sequence.fourth]] += 1
           final_sequences << sequence
         end
       end
@@ -58,11 +53,12 @@ end
     
 
 sequences.each do |sequence|
-  
+  add_sequence(final_sequences, final_frequencies,sequence)
+end
 
-puts "Number of sequences: #{sequences.length}"
+puts "Number of sequences: #{final_sequences.length}"
 
-puts "Frequencies: #{frequencies.inspect}"
+puts "Frequencies: #{final_frequencies.inspect}"
 
 
 
