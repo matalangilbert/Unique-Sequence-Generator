1a9c4d9c2da101213ed3c11c6e1f26f2a2a01a24
Mat Gilbert <freefallertam@gmail.com>
2012-04-02 18:14:45 +0100

    Commented script

--- seq_gen.rb
+++ seq_gen.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
+=begin
+Author:   Mathew Gilbert freefallertam _AT_ gmail _DOT_ com
+Filename: seq_gen.rb
+Date:     2-April-2012
+Summary:  Calculates unique 4 digit long number sequences where
+          each position (1 through 4) contains digits (0 through 9)
+          the same number of times. Outputs a .csv file for each sequence.
+          Calculates sequences from 10 digits long to 1000 digits long (starts
+          running more slowly after 100 digits long).
+=end
 
+=begin
+Input:    sequences => an array of sequences 4 digits long
+Returns:  two-dimensional array [position][digit] = frequency of digit occurrence
+=end
 def calcuate_frequencies(sequences)
   freq_array = Array.new(4) {|i|Array.new(10){|i| 0}}
   sequences.each do |seq|
@@ -10,18 +24,33 @@ def calcuate_frequencies(sequences)
   freq_array
 end
 
+=begin
+Returns:  array of all permutations of numbers 0 to 9, 4 digits long
+=end
 def generate_sequence()
   samples = (0..9).to_a.permutation(4).to_a
 end
 
+=begin
+Input:    frequencies => two-dimensional array ([position][digit] = frequency of digit occurrence)
+          digit => the digit to be queried (1 through 4)
+          position => the position of the digit to be queried
+Returns:  the frequency of occurrence of the digit at position
+=end
 def get_frequency(frequencies,digit,position)
   frequencies[position][digit]
 end
 
+=begin
+Main script. 
+Steps through sequences from 10 to 1000 in intervals of 10, outputs each
+unique sequence to a .csv file (number_of_sequences)_unique_sequences.csv
+Not presently optimised for speed.
+=end
 (10..1000).step(10) do |number_of_sequences|
   puts "Currently calculating #{number_of_sequences} sequences.."
+  @start_time = Time.new
   @sequences = generate_sequence()
-  #number_of_sequences = 50
   @final_sequences = Array.new
   while (@final_sequences.length<number_of_sequences)
     
@@ -52,15 +81,11 @@ end
       end
     end
   end
-  puts "#{number_of_sequences} Frequencies: #{calcuate_frequencies(@final_sequences)}"
-  puts "#{number_of_sequences} Length: #{@final_sequences.length}\n#{number_of_sequences} Sequences: #{@final_sequences}"
 
   File.open("#{number_of_sequences}_unique_sequences.csv","w") do |f|
       @final_sequences.each do |sequence|
       f.puts sequence.inspect.delete!("[] ")
     end
   end
-
+  puts "Completed in: #{Time.now-@start_time}"
 end
-puts "Final Frequencies: #{calcuate_frequencies(@final_sequences)}"
-puts "Final Length: #{@final_sequences.length}\nFinal Sequences: #{@final_sequences}"
