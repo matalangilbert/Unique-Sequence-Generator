8a0a79cfb2eaeb1e3f1c6db95c77a8da43bbdf56
Mat Gilbert <freefallertam@gmail.com>
2012-04-02 16:26:42 +0100

    Seems to be working

--- seq_gen.rb
+++ seq_gen.rb
@@ -1,91 +1,54 @@
 #!/usr/bin/env ruby
 
-def delete_from_samples(number, samples)
-  for i in 0..samples.length
-    if (samples[i] == number)
-      samples.delete_at(i)
-      break
-    end
-  end
-end
-
-def new_sample(first_samples, second_samples, third_samples, fourth_samples)
-  one = first_samples.first
-  two = nil
-  three = nil
-  four = nil
-  
-  second_samples.each do |second|
-    unless (second == one)
-      two = second
-    end
-  end
-  
-  third_samples.each do |third|
-    unless (third == two || third == one)
-      three = third
-    end
-  end
-
-  fourth_samples.each do |fourth|
-    unless ((fourth == three) || (fourth == two) || (fourth == one))
-      four = fourth
-    end
-  end
-   
-  unless (one.nil? || two.nil? || three.nil? or four.nil?)
-    sample = "#{one}#{two}#{three}#{four}"
-    return sample
-  end
-  nil
-end
-
 def calcuate_frequencies(sequences)
   freq_array = Array.new(4) {|i|Array.new(10){|i| 0}}
   sequences.each do |seq|
-    unless (seq.nil?)
-      array = seq.chars.to_a
-      for position in 0..array.length-1
-        freq_array[position][array[position].to_i] += 1
+      for position in 0..3
+        freq_array[position][seq[position].to_i] += 1
       end
-    end
   end
   freq_array
 end
 
 def generate_sequence()
-  begin
-    sequences = Array.new
-    first_position_samples = (0..9).to_a
-    second_position_samples = (0..9).to_a
-    third_position_samples =  (0..9).to_a
-    fourth_position_samples =  (0..9).to_a
+  samples = (0..9).to_a.permutation(4).to_a
+end
+
+def get_frequency(frequencies,digit,position)
+  frequencies[position][digit]
+end
+
+@sequences = generate_sequence()
+number_of_sequences = 50
+@final_sequences = Array.new
+while (@final_sequences.length<number_of_sequences)
+  
+  @test_sequences = Array.new 
+  @final_sequences = Array.new
+  
+  @sequences.shuffle!
+  @frequencies = calcuate_frequencies(@sequences)
+  
+  @sequences.each do |sequence|
+    sequence_ok = true
+    @test_sequences << sequence
     
-    0.upto(first_position_samples.length) do
-      sample = new_sample(first_position_samples, second_position_samples, third_position_samples, fourth_position_samples)
-      unless (sequences.include?(sample))
-          sequences << sample
-          puts "Sample: #{sample}. Total sequences: #{sequences.length}"
+    @test_frequencies = calcuate_frequencies(@test_sequences)
+    #puts calcuate_frequencies(@final_sequences).inspect
+    
+    for digit in 0..9
+      for position in 0..3      
+        if (get_frequency(@test_frequencies,digit,position)>number_of_sequences/10)
+          sequence_ok = false
+        end
       end
     end
-    sequences.compact!
-  end while (sequences.length<100)
-  return sequences
+    if sequence_ok
+      @final_sequences = @test_sequences.dup
+    else
+      @test_sequences.delete(sequence)
+    end
+  end
 end
-  
-@sequences = generate_sequence()
-
-unless @sequences.uniq!
-  puts "All unique sequences!"
-end
-
-freqs = calcuate_frequencies(@sequences)
-
-puts "Frequency 1 @ pos 1: #{freqs[0][0]}"
-puts "Frequency 2 @ pos 1: #{freqs[0][1]}"
-puts "Frequency 3 @ pos 1: #{freqs[0][2]}"
-
-puts ("Frequencies: #{freqs.inspect}")
-puts "Sequences: #{@sequences.inspect}"
-puts "Number of Sequences: #{@sequences.length}"
-
+puts "Final Frequencies: #{calcuate_frequencies(@final_sequences)}"
+puts "Final Length: #{@final_sequences.length}\nFinal Sequences: #{@final_sequences}"
